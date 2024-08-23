63f219f93c234151754c1a94e0098f374d2e688d
Dandi Rasyid <dandirasyid2003@gmail.com>
2024-08-24 01:22:14 +0700

    update php-dasar: aritmatika

--- php-dasar/aritmatika.php
+++ php-dasar/aritmatika.php
@@ -1,2 +1,31 @@
 <?php
 
+// Penjumlahan
+$a = 20;
+$b = 30;
+$result = $a + $b;
+var_dump($result);
+
+// Pengurangan
+$c = 100;
+$d = 40;
+$resultSubtraction = $d - $c;
+var_dump($resultSubtraction);
+$resultSubtractionPositive = -$resultSubtraction;
+var_dump($resultSubtractionPositive);
+
+// Memberikan hasil variabel menjadi negatif
+$resultNegative = -$result;
+var_dump($resultNegative);
+
+// Memberikan hasil variabel menjadi positif
+$resultPositive = +$result;
+var_dump($resultPositive);
+
+// Modulo atau sisa bagi
+$modulo = 100 % 3;
+var_dump($modulo);
+
+// Pangkat
+$pangkat = 100 ** 3;
+var_dump($pangkat);
